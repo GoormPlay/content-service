9d20809598f8605500cb439fc8fac32783369037
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-06-10 11:52:15 +0900

    Update SecurityConfig.java

--- src/main/java/com/goormplay/contentservice/Security/SecurityConfig.java
+++ src/main/java/com/goormplay/contentservice/Security/SecurityConfig.java
@@ -6,15 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,12 +23,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll() // 🔐 모든 요청 허용 (Gateway에서 이미 검증)
                 )
-        ;
+                .addFilterBefore(new JwtParsingFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
